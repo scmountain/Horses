967f3f4ff8966a6555c2e0336353084f44c345a7
Spencer Carter <scmountain@gmail.com>
2017-01-29 22:34:53 -0700

    style edits

--- spec/features/user_sees_links_spec.rb
+++ spec/features/user_sees_links_spec.rb
@@ -3,13 +3,13 @@ require_relative '../spec_helper'
 describe "when a user visits jockeys" do
   it "they see horse list" do
     visit("/")
-    click_on "View Horses"
+  click_on "View Horses"
     expect(current_path).to eql('/horses')
   end
 
   it "they see jockeys list" do
     visit("/")
-    click_on "View Jockeys"
+  click_on "View Jockeys"
     expect(current_path).to eql('/jockeys')
   end
 end
